c5fb232b80583a1dc117ba97b2466144cba9873e
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2023-10-19 14:06:41 +0630

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/digital-dye-studio",
   plugins: [react()],
-})
+  build: {
+    assetsInclude: [
+      "**/*.png",
+      "**/*.jpg",
+      "**/*.svg",
+      "**/*.ico",
+      "**/*.glb",
+      "**/*.gltf",
+    ],
+  },
+});
